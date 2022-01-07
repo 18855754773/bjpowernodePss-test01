2983a8ea16dd699654c3afb02964e50a488ebd15
pss <pss@qq.com>
2022-01-07 17:45:35 +0800

    123123

--- src/main/java/com/bjpowernode/domain/Student.java
+++ src/main/java/com/bjpowernode/domain/Student.java
@@ -4,13 +4,15 @@ public class Student {
 
     private String name;
     private int age;
+    private String id;
 
     public Student() {
     }
 
-    public Student(String name, int age) {
+    public Student(String name, int age,String id) {
         this.name = name;
         this.age = age;
+        this.id = id;
     }
 
     public String getName() {
@@ -27,5 +29,13 @@ public class Student {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
